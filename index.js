cb034a2e57b32089a8072255c9dacd8ce3ebc81e
Robert Wells <rwells@modelab.is>
2016-10-07 10:07:54 -0400

    add source

---
+++ index.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+var ENCODING = 'utf8';
+var INDEX = './index.js';
+
+var filedata = fs.readFileSync(INDEX, ENCODING);
+
+eval(filedata);
+
+exports.ClipperLib = ClipperLib;
